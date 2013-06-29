87f93c7f0a95306533bc4c9ba3f763c66c1b8f7e
José Mota <jose@josemota.net>
2013-06-29 12:50:11 +0100

    Setup single page in Rails.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Cookingember::Application.routes.draw do
+  root to: "app#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
